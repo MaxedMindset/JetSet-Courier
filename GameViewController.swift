bf74acbea94587296bf1517e12fdffbb2d5538a2
Max <127038155+MaxedMindset@users.noreply.github.com>
2025-02-14 21:12:16 +0100

    Update GameViewController.swift

--- GameViewController.swift
+++ GameViewController.swift
@@ -1,11 +1,3 @@
-//
-//  GameViewController.swift
-//  SkyCraftBuildAndFly
-//
-//  Erstellt von ChatGPT
-//  Lädt die erste Szene (MainMenuScene) in einem SKView
-//
-
 import UIKit
 import SpriteKit
 
@@ -14,23 +6,35 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Versuche, den SKView zu erhalten
         if let skView = self.view as? SKView {
-            let scene = MainMenuScene(size: skView.bounds.size)
+            // Erstelle die GameScene und konfiguriere sie
+            let scene = GameScene(size: skView.bounds.size)
             scene.scaleMode = .aspectFill
             skView.presentScene(scene)
             
-            // Debug-Optionen
             skView.ignoresSiblingOrder = true
+            
+            // FPS- und Knotenanzahl-Anzeige zum Debuggen
             skView.showsFPS = true
             skView.showsNodeCount = true
         }
     }
     
-    override func loadView() {
-        self.view = SKView(frame: UIScreen.main.bounds)
-    }
-    
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+}
+
+// Eine einfache GameScene zur Demonstration
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        backgroundColor = SKColor.black
+        
+        let label = SKLabelNode(text: "Game Started")
+        label.fontSize = 40
+        label.fontColor = SKColor.white
+        label.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        addChild(label)
     }
 }
